2147d8d6bde470d7d2e3b845b6a2bbf4cca92f94
Niks6455 <7929189niks64@gmail.com>
2022-12-14 19:29:58 +0300

    3

---
+++ front/src/components/common/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './Header.modules';
+
+const Header = () =>{
+    return (<header className={styles.header}></header>)
+}
+
+export default Header; 
---
+++ front/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from './../../common/Header/Header';
+
+const Home = () => {
+    return(
+        <div>
+            <Header />
+            <div>Home page</div>
+        </div>
+    )
+}
+
+export default Home;
